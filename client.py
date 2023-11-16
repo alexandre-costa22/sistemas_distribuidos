848079326e113358f93efd904bef9a32c325bb67
rafael-muller <79586899+rafael-muller@users.noreply.github.com>
2023-11-16 19:54:43 -0300

    Add files via upload
    Atualizado com pedido funcional.

--- client.py
+++ client.py
@@ -1,37 +1,151 @@
 import socket
+import pickle
 
-class ClienteTerminal:
-    def __init__(self, matricula, senha):
-        self.matricula = matricula
-        self.senha = senha
-        self.servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.servidor.connect(('localhost', 12345))
+def processar_pedido(matricula, total_pedido, usuarios):
+    if matricula in usuarios and usuarios[matricula]['autenticado']:
+        saldo_atual = usuarios[matricula]['saldo']
 
-    def autenticar(self):
-        mensagem = f"{self.matricula},{self.senha}"
-        self.servidor.send(mensagem.encode())
-        resposta = self.servidor.recv(1024).decode()
-        print(resposta)
+        if saldo_atual >= total_pedido:
+            usuarios[matricula]['saldo'] -= total_pedido
+            return "Compra realizada"
+        else:
+            return "Saldo Insuficiente"
+    else:
+        return "Usuário não autenticado. Faça login para fazer um pedido."
 
-    def fazer_pedido(self, pedido):
-        self.servidor.send(pedido.encode())
-        resposta = self.servidor.recv(1024).decode()
-        print(resposta)
+def cadastrar(matricula, senha, saldo=0):
+    return {'opcao': 'cadastrar', 'matricula': matricula, 'senha': str(senha), 'saldo': saldo}
 
-    def sair(self):
-        self.fazer_pedido("sair")
+def autenticar(matricula, senha, usuarios):
+    if matricula in usuarios and usuarios[matricula]['senha'] == senha:
+        usuarios[matricula]['autenticado'] = True
+        return {'opcao': 'autenticar', 'matricula': matricula, 'senha': senha, 'autenticado': True}
+    else:
+        return {'opcao': 'autenticar', 'matricula': matricula, 'senha': senha, 'autenticado': False}
+
+def adicionar_saldo(matricula, valor, usuarios):
+    if matricula in usuarios and usuarios[matricula]['autenticado']:
+        usuarios[matricula]['saldo'] += valor
+        return {'opcao': 'adicionar_saldo', 'matricula': matricula, 'valor': valor, 'novo_saldo': usuarios[matricula]['saldo']}
+    else:
+        return {'opcao': 'adicionar_saldo', 'matricula': matricula, 'valor': valor, 'erro': 'Usuário não autenticado'}
+
+def fazer_pedido(matricula, usuarios, client_socket):
+    print("1. Capuccino")
+    print("2. Mocaccino")
+    print("3. Chocolate Quente")
+    print("4. Chá Matte")
+    print("5. Chá Gelado ")
+    print("6. Salgado")
+    print("7. Doces")
+    total = float(0)
+    
+    while True:
+        print("Digite SAIR para sair")
+        pedido = input("Escolha seu pedido: ")
+        if pedido == '1':
+            total += 7
+        elif pedido == '2':
+            total += 8.50
+        elif pedido == '3':
+            total += 5.50
+        elif pedido == '4':
+            total += 8.50
+        elif pedido == '5':
+            total += 12
+        elif pedido == '6':
+            total += 15
+        elif pedido.lower() == 'sair':
+            print("SAINDO...")
+            break
+    
+    print(f'O seu total deu {total}')
+    opcao = 'fazer_pedido'
+    data = {'opcao': opcao, 'matricula': matricula, 'total_pedido': total}
+
+    print(f"Enviando dados para o servidor: {data}")
+    client_socket.send(pickle.dumps(data))
+    response = client_socket.recv(1024)
+
+    try:
+        response_dict = pickle.loads(response)
+
+        if response_dict.get('novo_saldo') is not None:
+            usuarios[matricula]['saldo'] = response_dict['novo_saldo']
+
+        print(response_dict['mensagem'])
+
+    except Exception as e:
+        print(f"Erro ao processar a resposta: {e}")
+    return data
+
+def main():
+    usuarios = {}
+
+    host = '127.0.0.1'
+    port = 12345
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    try:
+        while True:
+            print("1. Cadastro")
+            print("2. Autenticação")
+            print("3. Adicionar Fundos")
+            print("4. Fazer Pedidos")
+            print("5. Sair")
+            escolha = int(input("Escolha a opção: "))
+
+            if escolha == 1:
+                matricula = input("Matrícula: ")
+                senha = input("Senha: ")
+                saldo = float(input("Saldo (opcional): "))
+                data = cadastrar(matricula, senha, saldo=saldo)
+
+            elif escolha == 2:
+                matricula = input("Matrícula: ")
+                senha = input("Senha: ")
+                data = autenticar(matricula, senha, usuarios)
+
+            elif escolha == 3:
+                matricula = input("Matrícula: ")
+                valor = float(input("Valor a adicionar: "))
+                data = adicionar_saldo(matricula, valor, usuarios)
+
+            elif escolha == 4:
+                if 'autenticado' in usuarios.get(matricula, {}) and usuarios[matricula]['autenticado']:
+                    fazer_pedido(matricula, usuarios, client_socket)
+                else:
+                    print("Por favor, faça o login para realizar seu pedido!")
+                    continue
+
+            elif escolha == 5:
+                break
+
+            print(f"Enviando dados para o servidor: {data}")
+            client_socket.send(pickle.dumps(data))
+            response = client_socket.recv(1024)
+
+            try:
+                response_dict = pickle.loads(response)
+
+                if response_dict.get('autenticado'):
+                    usuarios[matricula] = {'autenticado': True, 'senha': senha, 'saldo': response_dict.get('novo_saldo')}
+                    print("Usuário autenticado com sucesso.")
+                elif 'erro' in response_dict:
+                    print(response_dict['erro'])
+                else:
+                    print("Operação concluída com sucesso.")
+
+            except Exception as e:
+                print(f"Erro ao processar a resposta: {e}")
+
+    except OSError as e:
+        print(f"Erro de conexão: {e}")
+
+    finally:
+        client_socket.close()
 
 if __name__ == "__main__":
-    matricula = input("Matrícula: ")
-    senha = input("Senha: ")
-
-    cliente = ClienteTerminal(matricula, senha)
-    cliente.autenticar()
-
-    while True:
-        pedido = input("Faça seu pedido (ou 'sair' para encerrar): ")
-        if pedido.lower() == 'sair':
-            cliente.sair()
-            break
-        else:
-            cliente.fazer_pedido(pedido)
+    main()
--- server.py
+++ server.py
@@ -1,64 +1,75 @@
 import socket
-import threading
-import queue
+import pickle
 
-class CantinaServer:
-    def __init__(self):
-        self.clientes_conectados = {}
-        self.fila_pedidos = queue.Queue()
-        self.mutex = threading.Lock()
+usuarios_cadastrados = []
 
-    def iniciar_servidor(self, host, porta):
-        self.servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.servidor.bind((host, porta))
-        self.servidor.listen(5)
-        print(f"Servidor iniciado em {host}:{porta}")
+def cadastrar_usuario(matricula, senha, saldo=0):
+    usuario = {'matricula': matricula, 'senha': senha, 'saldo': saldo, 'autenticado': False}
+    usuarios_cadastrados.append(usuario)
+    return usuario
 
-        while True:
-            cliente, endereco = self.servidor.accept()
-            threading.Thread(target=self.lidar_cliente, args=(cliente,)).start()
+def processar_pedido(data, usuarios):
+    opcao = data['opcao']
+    matricula = data['matricula']
 
-    def lidar_cliente(self, cliente):
-        matricula, senha = cliente.recv(1024).decode().split(',')
-        if self.autenticar_cliente(matricula, senha):
-            self.clientes_conectados[matricula] = cliente
-            cliente.send("Autenticado com sucesso!".encode())
-            self.enviar_cardapio(matricula)
-            self.lidar_pedidos(matricula)
+    if opcao == 'cadastrar':
+        senha = data['senha']
+        saldo = data.get('saldo', 0)
+        usuarios[matricula] = cadastrar_usuario(matricula, senha, saldo)
+        return {'mensagem': 'Usuário cadastrado com sucesso!'}
+
+    elif opcao == 'autenticar':
+        senha = data['senha']
+        for usuario in usuarios_cadastrados:
+            if matricula == usuario['matricula'] and usuario['senha'] == senha:
+                usuarios[matricula]['autenticado'] = True
+                return {'autenticado': True, 'novo_saldo': usuarios[matricula]['saldo']}
+        return {'autenticado': False, 'erro': 'Matrícula ou senha incorretos.'}
+
+    elif opcao == 'adicionar_saldo':
+        valor = data['valor']
+        usuarios[matricula]['saldo'] += valor
+        return {'mensagem': 'Saldo adicionado com sucesso!', 'novo_saldo': usuarios[matricula]['saldo']}
+
+    elif opcao == 'fazer_pedido':
+        total_pedido = data['total_pedido']
+
+        if usuarios[matricula]['saldo'] >= total_pedido:
+            usuarios[matricula]['saldo'] -= total_pedido
+            return {'mensagem': 'Pedido realizado com sucesso!', 'novo_saldo': usuarios[matricula]['saldo']}
         else:
-            cliente.send("Autenticação falhou!".encode())
+            return {'mensagem': 'Saldo insuficiente para realizar o pedido. Pedido cancelado.', 'novo_saldo': usuarios[matricula]['saldo']}
 
-    def autenticar_cliente(self, matricula, senha):
-        # Lógica de autenticação aqui
-        return True
 
-    def enviar_cardapio(self, matricula):
-        cardapio = "Cardápio:\n1. Produto 1\n2. Produto 2\n3. Produto 3\n"
-        self.clientes_conectados[matricula].send(cardapio.encode())
+def main():
+    usuarios = {}
+    host = '127.0.0.1'
+    port = 12345
 
-    def lidar_pedidos(self, matricula):
-        while True:
-            pedido = self.clientes_conectados[matricula].recv(1024).decode()
-            if pedido == "sair":
-                break
-            else:
-                self.mutex.acquire()
-                self.fila_pedidos.put((matricula, pedido))
-                self.mutex.release()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen()
 
-    def iniciar_atendimento(self):
-        while True:
-            if not self.fila_pedidos.empty():
-                pedido = self.fila_pedidos.get()
-                matricula, pedido_cliente = pedido
-                self.enviar_pedido_atendimento(matricula, pedido_cliente)
+    print(f"Servidor ouvindo em {host}:{port}")
 
-    def enviar_pedido_atendimento(self, matricula, pedido):
-        atendente = input("Atendente: ")
-        self.clientes_conectados[matricula].send(f"Atendente: {atendente}, Pedido: {pedido} concluído!".encode())
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Conexão estabelecida com {client_address}")
 
+        try:
+            while True:
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                data_dict = pickle.loads(data)
+                response = processar_pedido(data_dict, usuarios)
+                client_socket.send(pickle.dumps(response))
+
+        except Exception as e:
+            print(f"Erro na conexão: {e}")
+
+        finally:
+            client_socket.close()
 
 if __name__ == "__main__":
-    servidor = CantinaServer()
-    threading.Thread(target=servidor.iniciar_servidor, args=('localhost', 12345)).start()
-    threading.Thread(target=servidor.iniciar_atendimento).start()
+    main()
